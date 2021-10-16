d01103d13666ce7c464beb6457b0169f2f0241e2
nathanganesh <deeznutsgottem6969@gmail.com>
2021-10-16 16:25:55 +0200

    frontend auth fix

--- gettogetherfrontend/src/functions/auth.js
+++ gettogetherfrontend/src/functions/auth.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
-export const createUser = async (name,email,password,terms) =>{
-    return await axios.post(`http://localhost:8000/api/create-user`,{name,email,password,terms})
+export const createUser = async (name, email, password, terms) => {
+    return await axios.post(`http://localhost:8000/api/user/create-user`, {name, email, password, terms}, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
 }
