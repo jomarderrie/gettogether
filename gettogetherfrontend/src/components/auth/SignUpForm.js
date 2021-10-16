b420a5f5e7a63ff488d05377fe2b221e1914a25b
nathanganesh <deeznutsgottem6969@gmail.com>
2021-10-16 14:33:15 +0200

    Changed register button on signup page

--- gettogetherfrontend/src/components/auth/SignUpForm.js
+++ gettogetherfrontend/src/components/auth/SignUpForm.js
@@ -4,7 +4,10 @@ import ReCAPTCHA from "react-google-recaptcha";
 import {FlexBox} from "../../styled/styles";
 import styled from "styled-components";
 
+
+
 export default function SignUpForm() {
+
     const {
         register,
         handleSubmit,
@@ -18,67 +21,73 @@ export default function SignUpForm() {
 
     return (
         <form
-            onSubmit={handleSubmit(async (formData) => {
-                setSubmitting(true);
-                setServerErrors([]);
-
-                // const token = await reRef.current.executeAsync();
-                // reRef.current.reset();
-
-                const response = await fetch("/api/auth", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        name: formData.name,
-                        email: formData.email,
-                        password: formData.password,
-                        terms: formData.terms,
-                        // token,
-                    }),
-                });
-                const data = await response.json();
-
-                if (data.errors) {
-                    setServerErrors(data.errors);
-                } else {
-                    console.log("success, redirect to home page");
-                }
-
-                setSubmitting(false);
+            onSubmit={handleSubmit((data) => {
+                console.log(data);
             })}
+            // onSubmit={handleSubmit( (formData) => {
+            // console.log(formData)
+            // setSubmitting(true);
+            // setServerErrors([]);
+            //
+            // // const token = await reRef.current.executeAsync();
+            // // reRef.current.reset();
+            // let body = JSON.stringify({
+            //     name: formData.name,
+            //     email: formData.email,
+            //     password: formData.password,
+            //     terms: formData.terms,
+            //     // token,
+            // })
+            // console.log(body)
+            // const response = await fetch("/api/user/register", {
+            //     method: "POST",
+            //     headers: {
+            //         "Content-Type": "application/json",
+            //     },
+            //     body,
+            // });
+            // const data = await response.json();
+
+            // if (data.errors) {
+            //     setServerErrors(data.errors);
+            // } else {
+            //     console.log("success, redirect to home page");
+            // }
+            //
+            // setSubmitting(false);
+            // })}
         >
             {/*<ReCAPTCHA*/}
             {/*    sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}*/}
             {/*    size="invisible"*/}
             {/*    // ref={reRef}*/}
             {/*/>*/}
-
-            {serverErrors && (
-                <ul>
-                    {serverErrors.map((error) => (
-                        <li key={error}>{error}</li>
-                    ))}
-                </ul>
-            )}
-            <FlexBoxContainerInput z={"column"}  y={"none"}>
+            <FlexBoxContainerInput>
+                {serverErrors && (
+                    <ul>
+                        {serverErrors.map((error) => (
+                            <li key={error}>{error}</li>
+                        ))}
+                    </ul>
+                )}
+            </FlexBoxContainerInput>
+            <FlexBoxContainerInput z={"column"} y={"none"}>
                 <label htmlFor="firstName">First Name</label>
                 <input
                     {...register("firstName", {required: "This is required"})}
                     id="firstName"
                 />
-                {errors.firstName && <p>{errors.firstName.message}</p>}
+                {errors.firstName && <p className={"error"}>{errors.firstName.message}</p>}
             </FlexBoxContainerInput>
-            <FlexBoxContainerInput z={"column"}  y={"none"}>
+            <FlexBoxContainerInput z={"column"} y={"none"}>
                 <label htmlFor="lastName">Last Name</label>
                 <input
                     {...register("lastName", {
                         required: "This is required",
-                        maxLength: {value: 4, message: "You exceeded the max length"}
+                        maxLength: {value: 16, message: "You exceeded the max length"}
                     })}
                 />
-                {errors.lastName && <p>{errors.lastName.message}</p>}
+                {errors.lastName && <p className={"error"}>{errors.lastName.message}</p>}
             </FlexBoxContainerInput>
 
             <FlexBoxContainerInput z={"column"} y={"none"}>
@@ -86,64 +95,65 @@ export default function SignUpForm() {
                 <label htmlFor="email">Email</label>
 
                 <input
+
                     {...register("email", {
                         required: "This is required",
-                        pattern: (value) =>{
+                        pattern: (value) => {
                             return (
-                                [/[a-z]/, /[A-Z]/, /[0-9]/, /[^a-zA-Z0-9]/].every((pattern) =>
-                                    pattern.test(value)
-                                ) || "must include lower, upper, number, and special chars"
+                                new RegExp("^[^@\\s]+@[^@\\s\\.]+\\.[^@\\.\\s]+$").test(value)
                             );
                         },
-                        maxLength: {value: 4, message: "Valid email"}
+                        minLength: {value: 6, message: "Valid email"}
                     })}
                 />
-                {errors.email && <p>{errors.email.message}</p>}
+                {errors.email && <p className={"error"}>{errors.email.message}</p>}
             </FlexBoxContainerInput>
-
 
             <FlexBoxContainerInput z={"column"} y={"none"}>
 
-                <label htmlFor="Password">Password</label>
+                <label htmlFor="password">Password</label>
 
                 <input
-                    {...register("email", {
+                    type={"password"}
+                    {...register("password", {
                         required: "This is required",
-                        pattern: (value) =>{
-                            return (
-                                [/[a-z]/, /[A-Z]/, /[0-9]/, /[^a-zA-Z0-9]/].every((pattern) =>
+                        minLength: {
+                            value: 8,
+                            message: "must be 8 chars",
+                        },
+                        validate: (value) => {
+                            return !([/[a-z]/, /[A-Z]/, /[0-9]/, /[^a-zA-Z0-9]/].every((pattern) =>
                                     pattern.test(value)
                                 ) || "must include lower, upper, number, and special chars"
                             );
                         },
-                        maxLength: {value: 4, message: "Valid email"}
                     })}
                 />
-                {errors.email && <p>{errors.email.message}</p>}
+                {errors.password && <p className={"error"}>{errors.password.message}</p>}
             </FlexBoxContainerInput>
 
 
-            <FlexBoxContainerInput  z={"row"} x={"flex-start"}>
-                <input
-                    type="checkbox"
-                    className={"checkbox-input"}
-                    {...register("terms",{required:"This is required"})}
+            {/*<FlexBoxContainerInput z={"row"} x={"flex-start"}>*/}
+            {/*    <input*/}
+            {/*        type="checkbox"*/}
+            {/*        className={"checkbox-input"}*/}
+            {/*        {...register("terms", {required: "This is required"})}*/}
 
-                    // type="checkbox"
-                    // name="terms"
-                    // id="terms"
-                    // ref={register({ required: "you must agree to terms" })}
-                />
-                <label htmlFor="terms" style={{paddingRight:"20px"}}>You must agree to our terms.</label>
-            </FlexBoxContainerInput>
-
-
+            {/*        // type="checkbox"*/}
+            {/*        // name="terms"*/}
+            {/*        // id="terms"*/}
+            {/*        // ref={register({ required: "you must agree to terms" })}*/}
+            {/*    />*/}
+            {/*    <label htmlFor="terms" style={{paddingLeft: "20px"}}>You must agree to our terms.</label>*/}
+            {/*</FlexBoxContainerInput>*/}
+            {/*{errors.terms && <p className={"error"}>{errors.terms.message}</p>}*/}
 
             <FlexBoxContainerInput z={"column"}>
-                <button type="submit" disabled={submitting}>
+                <button type="submit" className={"register-btn"}>
                     Register
                 </button>
-                <p>When you "Continue", you agree to GetTogether's Terms of Service. We will manage information about you as described in our Privacy Policy, and Cookie Policy.</p>
+                <p>When you "Continue", you agree to GetTogether's Terms of Service. We will manage information about
+                    you as described in our Privacy Policy, and Cookie Policy.</p>
             </FlexBoxContainerInput>
 
         </form>
@@ -152,21 +162,37 @@ export default function SignUpForm() {
 // width420
 
 
-
 const FlexBoxContainerInput = styled(FlexBox)`
   width: 640px;
   padding: 10px;
-  .checkbox{
+
+  .checkbox {
     padding-left: 20px;
   }
-  label{
+
+  label {
     color: #aba499;
   }
-  input{
-    padding: 10px 8px;
+
+  .error {
+    font-size: 14px;
+    color: red;
   }
-  p{
+
+  .register-btn {
+    width: 100%;
+    padding: 4px;
+    font-size: 24px;
+  }
+
+  input {
+    padding: 10px 8px;
+    font-size: 20px;
+  }
+
+  p {
     font-size: 10px;
+    padding: 10px 0px;
   }
 `
 
